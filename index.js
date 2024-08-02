bb31104846ecca359a9a90e148c4435ed54449aa
LieraBerh <lierrastern@gmail.com>
2024-08-02 14:22:10 +0200

    clean

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       const allContacts = await listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
 
     case "get":
       const oneContact = await getContactById(id);
